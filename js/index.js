469be14a184cc9832905537ef4238a31c4b900a7
Mushfikha <mushfikha0894@gmail.com>
2024-09-24 23:32:23 +0600

    FAQ section completed

--- js/index.js
+++ js/index.js
@@ -6,19 +6,30 @@ document.getElementById('donate-now-btn')
  .addEventListener('click', function(){
     const noakhaliInput = parseFloat(document.getElementById('noakhali-input-field').value);
    
-    if(!isNaN(noakhaliInput) && noakhaliInput !== ''){
+ 
        
         const nkDonationAmount = parseFloat(document.getElementById('nk-donation-amount').innerText);
         const newDonation = noakhaliInput + nkDonationAmount;
         document.getElementById('nk-donation-amount').innerText = newDonation.toFixed(2);
-    }
+    
 
-    else{
-     document.getElementById('noakhali-input-field').innerText = `${noakhaliInput} is not a valid number.`
-    }
-       
    
- 
+       
+ }); 
+   
 
- });
+
+  
+   
+    // if(!isNaN(noakhaliInput) && noakhaliInput !== ''){
+       
+       
+    // }
+
+    // else{
+    // alert('You Donated an invalid Amount!!!')
+    // }
+       
+
+
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,4 @@
+// button navigation function;
 const donateButton = document.getElementById('donate-btn');
  donateButton.addEventListener('click', function (){
     window.location.href = "../index.html"
@@ -7,4 +8,30 @@ const donateButton = document.getElementById('donate-btn');
   .addEventListener('click', function() {
      window.location.href = "../history.html";
   });
+
+  document.getElementById('btn-blog')
+  .addEventListener('click', function() {
+     window.location.href = "../blog.html";
+  });
+
+  document.getElementById('home-btn')
+  .addEventListener('click', function() {
+     window.location.href = "../index.html";
+ 
+  });
+
+
+
+
+
+
+// popup modal functions
+const myModal = document.getElementById("my-modal");
+
+function openPopup(){
+   myModal.classList.remove('hidden');
+};
+function closePopup(){
+   myModal.classList.add('hidden');
+};
 
